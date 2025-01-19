d868de462901dfb129384515bc7fdc746bb73a38
Nanda Gopalan <3970199+nandagopalan@users.noreply.github.com>
2025-01-19 21:43:01 +1100

    [#97] Refactored Context to ServerContext

--- rest/server_context.go
+++ rest/server_context.go
@@ -20,14 +20,14 @@ var xmlCodec = codec.XmlCodec()
 // yamlCodec is the default codec for yaml
 var yamlCodec = codec.YamlCodec()
 
-// Context is the struct that holds the request and response of the server.
-type Context struct {
+// ServerContext is the struct that holds the request and response of the server.
+type ServerContext struct {
 	request  *http.Request
 	response http.ResponseWriter
 }
 
 // Options is the struct that holds the configuration for the Server.
-func (c *Context) GetParam(name string, typ Paramtype) (string, error) {
+func (c *ServerContext) GetParam(name string, typ Paramtype) (string, error) {
 	switch typ {
 	case QueryParam:
 		return turbo.GetQueryParam(name, c.request)
@@ -39,17 +39,17 @@ func (c *Context) GetParam(name string, typ Paramtype) (string, error) {
 }
 
 // GetBody returns the body of the request.
-func (c *Context) GetBody() (io.Reader, error) {
+func (c *ServerContext) GetBody() (io.Reader, error) {
 	return c.request.Body, nil
 }
 
 // GetHeader returns the header of the request.
-func (c *Context) GetHeader(name string) string {
+func (c *ServerContext) GetHeader(name string) string {
 	return c.request.Header.Get(name)
 }
 
 // InHeaders returns the headers of the request.
-func (c *Context) InHeaders() http.Header {
+func (c *ServerContext) InHeaders() http.Header {
 	// clone the headers
 	headers := make(http.Header)
 	for k, v := range c.request.Header {
@@ -59,23 +59,23 @@ func (c *Context) InHeaders() http.Header {
 }
 
 // GetMethod returns the method of the request.
-func (c *Context) GetMethod() string {
+func (c *ServerContext) GetMethod() string {
 	return c.request.Method
 }
 
 // GetURL returns the URL of the request.
-func (c *Context) GetURL() string {
+func (c *ServerContext) GetURL() string {
 	return c.request.URL.String()
 }
 
 // GetRequest returns the request.
 // for most Rest Use cases this would not be required
-func (c *Context) GetRequest() *http.Request {
+func (c *ServerContext) GetRequest() *http.Request {
 	return c.request
 }
 
 // Read reads the body of the request into the given object.
-func (c *Context) Read(obj interface{}) error {
+func (c *ServerContext) Read(obj interface{}) error {
 	contentType := c.request.Header.Get(ContentTypeHeader)
 	codec, err := codec.GetDefault(contentType)
 	if err != nil {
@@ -86,25 +86,25 @@ func (c *Context) Read(obj interface{}) error {
 }
 
 // WriteJSON writes the object to the response in JSON format.
-func (c *Context) WriteJSON(data interface{}) error {
+func (c *ServerContext) WriteJSON(data interface{}) error {
 	c.SetHeader(ContentTypeHeader, ioutils.MimeApplicationJSON)
 	return jsonCodec.Write(data, c.response)
 }
 
 // WriteXML writes the object to the response in XML format.
-func (c *Context) WriteXML(data interface{}) error {
+func (c *ServerContext) WriteXML(data interface{}) error {
 	c.SetHeader(ContentTypeHeader, ioutils.MimeApplicationXML)
 	return xmlCodec.Write(data, c.response)
 }
 
 // WriteYAML writes the object to the response in YAML format.
-func (c *Context) WriteYAML(data interface{}) error {
+func (c *ServerContext) WriteYAML(data interface{}) error {
 	c.SetHeader(ContentTypeHeader, ioutils.MimeTextYAML)
 	return yamlCodec.Write(data, c.response)
 }
 
 // Write writes the object to the response with the given content type and status code.
-func (c *Context) Write(data interface{}, contentType string) error {
+func (c *ServerContext) Write(data interface{}, contentType string) error {
 	codec, err := codec.GetDefault(contentType)
 	if err != nil {
 		return err
@@ -114,42 +114,42 @@ func (c *Context) Write(data interface{}, contentType string) error {
 }
 
 // WriteData writes the data to the response.
-func (c *Context) WriteData(data []byte) (int, error) {
+func (c *ServerContext) WriteData(data []byte) (int, error) {
 	return c.response.Write(data)
 }
 
 // WriteString writes the string to the response.
-func (c *Context) WriteString(data string) {
+func (c *ServerContext) WriteString(data string) {
 
 	io.Copy(c.response, strings.NewReader(data))
 }
 
 // SetHeader sets the header of the response.
-func (c *Context) SetHeader(name, value string) {
+func (c *ServerContext) SetHeader(name, value string) {
 	c.response.Header().Set(name, value)
 }
 
 // SetContentType sets the content type of the response.
-func (c *Context) SetContentType(contentType string) {
+func (c *ServerContext) SetContentType(contentType string) {
 	c.response.Header().Set(ContentTypeHeader, contentType)
 }
 
 // SetStatusCode sets the status code of the response.
-func (c *Context) SetStatusCode(statusCode int) {
+func (c *ServerContext) SetStatusCode(statusCode int) {
 	c.response.WriteHeader(statusCode)
 }
 
 // SetCookie sets the cookie of the response.
-func (c *Context) SetCookie(cookie *http.Cookie) {
+func (c *ServerContext) SetCookie(cookie *http.Cookie) {
 	http.SetCookie(c.response, cookie)
 }
 
 // WriteFrom writes the data from the reader to the response.
-func (c *Context) WriteFrom(data io.Reader) {
+func (c *ServerContext) WriteFrom(data io.Reader) {
 	io.Copy(c.response, data)
 }
 
 // HttpResWriter returns the http.ResponseWriter
-func (c *Context) HttpResWriter() http.ResponseWriter {
+func (c *ServerContext) HttpResWriter() http.ResponseWriter {
 	return c.response
 }
