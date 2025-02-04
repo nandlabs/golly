5cf4669ea5307b089d94d2a3a0c52b08066b44f0
Aditya Kumar <adityak007mr@gmail.com>
2025-02-04 20:26:05 +0530

    refactored cli module for a simplified implementation, [#99] added support for command specific flags

--- cli/command.go
+++ cli/command.go
@@ -1,146 +1,23 @@
 package cli
 
-import (
-	"errors"
-	"flag"
-)
-
 type Command struct {
-	// command name used to invoke from CLI
-	Name string
-	// command usage information
-	Usage     string
-	ArgsUsage string
-	// the array of aliases to invoke the commands
-	Aliases []string
-	// execute on the command invocation
-	Action ActionFunc
-	// command specific flags
-	Flags []*Flag
-	// subcommands of the root command
-	Commands             []*Command
-	HelpName             string
-	UsageText            string
-	SubCommandsAvailable bool
+	Name        string
+	Description string
+	Handler     func(ctx *Context) error
+	SubCommands map[string]*Command
+	Flags       []Flag
 }
 
-func (command *Command) Run(conTxt *Context, arguments ...string) error {
-	a := args(arguments)
-	output := a.FetchArgs()
-	inputArgs := output.inputCommands
-	inputFlags := output.inputFlags
-
-	command.addUserDefinedFlags(inputFlags)
-	parseArgs()
-
-	isHelpPresent := a.checkForHelp()
-	var finalCommand *Command
-
-	if len(inputArgs) > 0 {
-		finalCommand = command.findCommandPath(conTxt, inputArgs)
-		if finalCommand == nil {
-			return errors.New("command not found")
-		}
-		command.Action = finalCommand.Action
-		conTxt.Command = finalCommand
+func NewCommand(name, description string, handler func(ctx *Context) error) *Command {
+	return &Command{
+		Name:        name,
+		Description: description,
+		Handler:     handler,
+		SubCommands: make(map[string]*Command),
+		Flags:       []Flag{},
 	}
-
-	if len(conTxt.Command.Commands) > 0 {
-		conTxt.Command.SubCommandsAvailable = true
-	}
-
-	if isHelpPresent {
-		return helpCommand.Action(conTxt)
-	}
-
-	if command.Action == nil {
-		command.Action = helpCommand.Action
-	}
-
-	err := command.Action(conTxt)
-	return err
 }
 
-// with default flag library they can be parsed if they are added before args
-func parseArgs() {
-	flag.Parse()
-	flag.VisitAll(func(f *flag.Flag) {
-		if f.Value != nil {
-			mappedFlags[f.Name] = f.Value
-		} else {
-			mappedFlags[f.Name] = f.DefValue
-		}
-	})
-}
-
-func (command *Command) allFlags() []*Flag {
-	var flags []*Flag
-	flags = append(flags, command.Flags...)
-	return flags
-}
-
-func (command *Command) HasName(name string) bool {
-	for _, n := range command.Names() {
-		if n == name {
-			return true
-		}
-	}
-	return false
-}
-
-func (command *Command) Names() []string {
-	return append([]string{command.Name}, command.Aliases...)
-}
-
-func hasCommand(commands []*Command, command *Command) bool {
-	for _, exist := range commands {
-		if command == exist {
-			return true
-		}
-	}
-	return false
-}
-
-func (command *Command) findCommandPath(conTxt *Context, args []string) *Command {
-	for _, c := range command.Commands {
-		found := search(c, conTxt, args)
-		if found != nil {
-			return found
-		}
-	}
-	return nil
-}
-
-func search(command *Command, conTxt *Context, args []string) *Command {
-	if command == nil {
-		return nil
-	}
-	if command.Name == args[0] {
-		if len(args) == 1 {
-			return command
-		}
-		for _, child := range command.Commands {
-			if search(child, conTxt, args[1:]) != nil {
-				return child
-			}
-		}
-	}
-	return nil
-}
-
-func (command *Command) checkForAlias(arg string) bool {
-	for _, a := range command.Aliases {
-		if arg == a {
-			return true
-		}
-	}
-	return false
-}
-
-func (command *Command) addUserDefinedFlags(inputFlags []string) {
-	setFlags(command.Flags, inputFlags)
-}
-
-func (command *Command) VisibleCommands() []*Command {
-	return command.Commands
+func (cmd *Command) AddSubCommand(subCmd *Command) {
+	cmd.SubCommands[subCmd.Name] = subCmd
 }
---
+++ cli/usage.go
@@ -0,0 +1,79 @@
+package cli
+
+import (
+	"fmt"
+	"strings"
+)
+
+// func (cli *CLI) printUsage() {
+// 	fmt.Println("CLI Tool Usage:")
+// 	fmt.Println("Available Commands:")
+// 	for _, cmd := range cli.rootCommands {
+// 		cli.printCommandHelp(cmd, 1)
+// 	}
+// }
+
+// func (cli *CLI) printCommandHelp(cmd *Command, indent int) {
+// 	indentation := strings.Repeat("  ", indent)
+// 	fmt.Printf("%s%s: %s\n", indentation, cmd.Name, cmd.Description)
+// 	for _, flag := range cmd.Flags {
+// 		fmt.Printf("%s  --%s (aliases: %v): %s [default: %s]\n", indentation, flag.Name, flag.Aliases, flag.Usage, flag.Default)
+// 	}
+// 	for _, subCmd := range cmd.SubCommands {
+// 		cli.printCommandHelp(subCmd, indent+1)
+// 	}
+// }
+
+func (cli *CLI) printUsage() {
+	fmt.Println("\nCLI Tool Usage:")
+	fmt.Println("Usage: [command] [subcommand] [flags]")
+	fmt.Println("\nGlobal Flags:")
+	fmt.Println("  -h, --help          Show help for the CLI tool")
+
+	fmt.Println("\nAvailable Commands:")
+	for _, cmd := range cli.rootCommands {
+		cli.printCommandHelp(cmd, 1)
+	}
+	fmt.Println()
+}
+
+func (cli *CLI) printCommandHelp(cmd *Command, indent int) {
+	indentation := strings.Repeat("  ", indent)
+	fmt.Printf("%s%s: %s\n", indentation, cmd.Name, cmd.Description)
+
+	if len(cmd.Flags) > 0 {
+		fmt.Printf("%sFlags:\n", indentation)
+		for _, flag := range cmd.Flags {
+			fmt.Printf("%s  --%s (aliases: %v): %s [default: %s]\n", indentation, flag.Name, flag.Aliases, flag.Usage, flag.Default)
+		}
+	}
+
+	if len(cmd.SubCommands) > 0 {
+		fmt.Printf("%sSubcommands:\n", indentation)
+		for _, subCmd := range cmd.SubCommands {
+			cli.printCommandHelp(subCmd, indent+1)
+		}
+	}
+}
+
+func (cli *CLI) printDetailedHelp(commandStack []string, cmd *Command) {
+	fmt.Printf("\nHelp for Command: %s\n", strings.Join(commandStack, " "))
+	fmt.Printf("\n%s: %s\n", cmd.Name, cmd.Description)
+	fmt.Println("\nUsage:")
+	fmt.Printf("  %s [flags]\n", strings.Join(commandStack, " "))
+
+	if len(cmd.Flags) > 0 {
+		fmt.Println("\nFlags:")
+		for _, flag := range cmd.Flags {
+			fmt.Printf("  --%s (aliases: %v): %s [default: %s]\n", flag.Name, flag.Aliases, flag.Usage, flag.Default)
+		}
+	}
+
+	if len(cmd.SubCommands) > 0 {
+		fmt.Println("\nSubcommands:")
+		for _, subCmd := range cmd.SubCommands {
+			fmt.Printf("  %s: %s\n", subCmd.Name, subCmd.Description)
+		}
+	}
+	fmt.Println()
+}
